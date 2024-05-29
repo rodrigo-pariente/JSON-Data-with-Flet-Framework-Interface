ec117977fe9848e031537ef6cd25285d7ab2f6a8
rodrigo pariente <rodrigomanedio17@gmail.com>
2024-05-29 16:52:05 -0400

    Auto commit

--- files/complex_data_item_replacer.py
+++ files/complex_data_item_replacer.py
@@ -1,5 +1,5 @@
-from ui_components import ListDropdown, DictDropdown, ValueTextField
 import json
+from utils import starts_and_ends_with, convert_number, is_valid_json_list_or_dict
 from typing import Union
 
 def path_treatment(path):
@@ -71,47 +71,26 @@ def complex_data_item_replacer(data: Union[list, dict, str, int, float], path: s
     # Retorna a estrutura de dados original se a chave não for encontrada
     return data
 
-def starts_and_ends_with(string: str, circumfix: str):
-    return (string.startswith(circumfix) and string.endswith(circumfix))
+dados_complexos = [
+    "primeiro elemento",
+    {'chave_dict_1': 'valor_dict_1'},
+    [
+        {'chave_dict_lista': [1, 2, 3]},
+        "elemento_lista_2",
+        {'outra_chave_dict_lista': {'sub_lista': [4.5, 6.7, 8.9]}},
+        100,
+        "último_elemento_lista"
+    ],
+    "último elemento"
+]
 
-def convert_number(number_str: str):
-    try:
-        return int(number_str)
-    except ValueError:
-        try:
-            return float(number_str)
-        except ValueError:
-            return number_str
- 
-def all_options_primitive(dropdown) -> bool:
-    for option in dropdown.options:
-        if isinstance(dropdown, ListDropdown):
-            if is_valid_json_list_or_dict(option.key) or isinstance(option.key, (list, dict)):
-                return False
-        else:
-            if is_valid_json_list_or_dict(dropdown.dictionary[option.key]) or isinstance(dropdown.dictionary[option.key], (list, dict)):
-                return False
-    return True
-
-def is_valid_json_list_or_dict(string: str) -> bool:
-    try:
-        data = json.loads(string)
-        return isinstance(data, (list, dict))
-    except (json.decoder.JSONDecodeError, TypeError):
-        return False
-
-def create_child_for_list(data) -> ListDropdown: 
-    return ListDropdown(data)
-
-def create_child_for_dict(data) -> DictDropdown:
-    return DictDropdown(data)
-
-def create_child_for_value(value) -> ValueTextField:
-    return ValueTextField(value)
-
-def ui_component(data):
-    if isinstance(data, list):
-        return create_child_for_list(data)
-    elif isinstance(data, dict):
-        return create_child_for_dict(data)
-    return create_child_for_value(data)
+path_4 = """[
+        {'chave_dict_lista': [1, 2, 3]},
+        "elemento_lista_2",
+        {'outra_chave_dict_lista': {'sub_lista': [4.5, 6.7, 8.9]}},
+        100,
+        "último_elemento_lista"
+    ]/{'chave_dict_lista': [1, 2, 3]}/'chave_dict_lista'/2"""
+new_value_4 = "boing 787"
+modified = complex_data_item_replacer(dados_complexos, path_4, new_value_4, false_if_not_found=True)
+print(modified)
